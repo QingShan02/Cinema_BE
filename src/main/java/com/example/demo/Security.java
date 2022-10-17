935718c827f87be490c475144deb4c376f823e74
khaiminh0401 <khaiminh0401@gmail.com>
2022-10-17 23:34:07 +0700

    hehe

---
+++ src/main/java/com/example/demo/Security.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+
+/*import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;*/
+//@Configuration
+//@EnableWebSecurity
+public class Security {
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		// TODO Auto-generated method stub
+////		http.authorizeRequests()
+////			.antMatchers("/api/*").permitAll();
+////		http.
+////		http.authorizeRequests().antMatchers("/main/*").authenticated().anyRequest().authenticated().and().formLogin();
+//		super.configure(http);
+//	}
+
+}
