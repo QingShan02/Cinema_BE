c6da9210b4710576446e618007fddba825677384
Khang Ho <hohoangkhang18@gmail.com>
2022-10-23 18:10:46 +0700

    update topping

--- bin/src/main/resources/static/Ve/Service1.js
+++ bin/src/main/resources/static/Ve/Service1.js
@@ -1,7 +1,12 @@
-class  Service1{
-    fillTable = async() =>{
-        let {data:result} = await axios.get("/api/ve/getAllVe");
+class Service1 {
+    fillTable = async() => {
+        let { data: result } = await axios.get("/api/ve/getAllVe");
         $$('dashboard1').parse(result);
     }
+    fillphim = async() => {
+        let { data: result } = await axios.get("/api/phim/getAllPhim");
+        $$('combo1').parse(result);
+    }
 }
+
 export default new Service1();
