bb5cebddce77f3eaeff3c24d054800e0bf02b279
Tra1507 <traptps21791@fpt.edu.vn>
2022-11-23 13:45:12 +0700

    chitietphim - lichchieu

--- src/main/java/com/example/demo/ResController/ApiChiNhanh.java
+++ src/main/java/com/example/demo/ResController/ApiChiNhanh.java
@@ -4,6 +4,7 @@ import com.example.demo.model.ChiNhanh;
 import com.example.demo.service.ChiNhanhService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/chinhanh")
+
+@CrossOrigin("*")
+
 public class ApiChiNhanh {
     @Autowired
     ChiNhanhService service;
